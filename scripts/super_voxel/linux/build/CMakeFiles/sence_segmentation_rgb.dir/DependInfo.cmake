94183a0303227893f073f80662abaa9f5f45b132
usiege <2275572017@qq.com>
2020-05-15 00:48:32 +0800

    super voxel merge

---
+++ scripts/super_voxel/linux/build/CMakeFiles/sence_segmentation_rgb.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/charles/work/working/PointCloud/scripts/super_voxel-based/linux/file_trs.cpp" "/home/charles/work/working/PointCloud/scripts/super_voxel-based/linux/build/CMakeFiles/sence_segmentation_rgb.dir/file_trs.cpp.o"
+  "/home/charles/work/working/PointCloud/scripts/super_voxel-based/linux/segment_process.cpp" "/home/charles/work/working/PointCloud/scripts/super_voxel-based/linux/build/CMakeFiles/sence_segmentation_rgb.dir/segment_process.cpp.o"
+  "/home/charles/work/working/PointCloud/scripts/super_voxel-based/linux/sence_segmentation_rgb.cpp" "/home/charles/work/working/PointCloud/scripts/super_voxel-based/linux/build/CMakeFiles/sence_segmentation_rgb.dir/sence_segmentation_rgb.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DISABLE_DAVIDSDK"
+  "DISABLE_DSSDK"
+  "DISABLE_ENSENSO"
+  "DISABLE_LIBUSB_1_0"
+  "DISABLE_OPENNI"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "DISABLE_QHULL"
+  "DISABLE_VTK"
+  "EIGEN_USE_NEW_STDVECTOR"
+  "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
+  "FLANN_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/pcl-1.8"
+  "/usr/include/eigen3"
+  "/usr/include/openni2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ scripts/super_voxel/linux/build/CMakeFiles/sence_segmentation_rgb.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sence_segmentation_rgb.dir/sence_segmentation_rgb.cpp.o"
+  "CMakeFiles/sence_segmentation_rgb.dir/segment_process.cpp.o"
+  "CMakeFiles/sence_segmentation_rgb.dir/file_trs.cpp.o"
+  "sence_segmentation_rgb.pdb"
+  "sence_segmentation_rgb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sence_segmentation_rgb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
